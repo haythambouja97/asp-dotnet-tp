3452e8455b8327de7d4640abc63c21367427cf3b
haythambouja97 <hbouja97@gmail.com>
2022-03-19 18:26:58 +0100

    first commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAppEmploye.Models.Repositories;
+using WebApplicationEmployé.Models;
+using WebApplicationEmployé.Models.Repositories;
+
+namespace WebApplicationEmployé.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        readonly IRepository<Employee> employeeRepository;
+        //injection de dépendance
+        public EmployeeController(IRepository<Employee> empRepository)
+        {
+            employeeRepository = empRepository;
+        }
+        // GET: EmployeeController
+        public ActionResult Index()
+        {
+            var employees = employeeRepository.GetAll();
+            ViewData["EmployeesCount"] = employees.Count();
+            ViewData["SalaryAverage"] = employeeRepository.SalaryAverage();
+            ViewData["MaxSalary"] = employeeRepository.MaxSalary();
+            ViewData["HREmployeesCount"] = employeeRepository.HrEmployeesCount();
+            return View(employees);
+        }
+
+        // GET: EmployeeController/Details/5
+        public ActionResult Details(int id)
+        {
+            var employee = employeeRepository.FindByID(id);
+            return View(employee);
+
+        }
+
+        // GET: EmployeeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employee emp)
+        {
+            try
+            {
+                employeeRepository.Add(emp);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Employee emp)
+        {
+            try
+            {
+                employeeRepository.Update(id,emp);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+        // POST: EmployeeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                employeeRepository.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult Search(string term)
+        {
+            var result = employeeRepository.Search(term);
+            return View("Index", result);
+        }
+
+
+    }
+}
---
+++ Models/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplicationEmployé.Models.Repositories
+{
+    public interface IRepository<T>
+    {
+        double SalaryAverage();
+        double MaxSalary();
+        int HrEmployeesCount();
+
+        List<T> Search(string term);
+        IList<T> GetAll();
+        T FindByID(int id);
+        void Add(T entity);
+        void Update(int id, T entity);
+        void Delete(int id);
+    }
+}
